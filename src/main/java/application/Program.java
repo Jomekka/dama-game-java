da5d7248dba9d8c6529de5f8d5bc74e811f609f7
Lucas <lucasmeirelesdacosta@gmail.com>
2020-08-19 23:52:31 -0300

    Primeira Classe Criada

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,11 +1,15 @@
 package application;
+
+import Tabuleiro.Posicao;
+
 public class Program {
     
     public static void main(String [] args){
         System.out.println("Hello");
         
+        Posicao d = new Posicao(3,5);
         
-        
+        System.out.println(d);
         
         
         
